5139eb4335bf40bade4382d3f780d626a4436f63
ttphu2 <ttphu34@gmail.com>
2021-02-15 17:11:34 +0700

    Add thu vien xng-breadcrumb va Add breadcrumb vao project

--- client/src/app/shop/product-details/product-details.component.ts
+++ client/src/app/shop/product-details/product-details.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { IProduct } from 'src/app/shared/models/product';
+import { BreadcrumbService } from 'xng-breadcrumb';
 import { ShopService } from '../shop.service';
 
 @Component({
@@ -10,7 +11,7 @@ import { ShopService } from '../shop.service';
 })
 export class ProductDetailsComponent implements OnInit {
   product: IProduct;
-  constructor(private shopService: ShopService, private activeRoute: ActivatedRoute) { }
+  constructor(private shopService: ShopService, private activeRoute: ActivatedRoute,private bcService: BreadcrumbService) { }
 
   ngOnInit(): void {
     this.loadProduct();
@@ -19,7 +20,7 @@ export class ProductDetailsComponent implements OnInit {
   loadProduct(){
     this.shopService.getProduct(Number(this.activeRoute.snapshot.paramMap.get('id'))).subscribe(product => {
       this.product=product;
-    
+      this.bcService.set('@productDetails',product.name);
     }, error => {
       console.log(error);
     })
