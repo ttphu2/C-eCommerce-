711a0c09858e11dbe439a7e22083abda7582c1b5
ttphu2 <ttphu34@gmail.com>
2021-02-18 15:21:21 +0700

    1. Cai dat redis,chocolately 2. BasketController

---
+++ API/Controllers/BasketController.cs
@@ -0,0 +1,39 @@
+using System.Threading.Tasks;
+using Core.Entities;
+using Core.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    //Power shell vs quyen AD => choco install redis-64 --version 3.0.503
+    //redis-server
+    //redis-cli KEYS *
+    //redis-cli GET basket1
+
+    public class BasketController : BaseApiController
+    {
+        private readonly IBasketRepository _basketRepository;
+        public BasketController(IBasketRepository basketRepository)
+        {
+            _basketRepository = basketRepository;
+
+        }
+        [HttpGet]
+        public async Task<ActionResult<CustomerBasket>> GetBasketById(string id){
+            var basket = await _basketRepository.GetBasketAsync(id);
+            return Ok(basket ?? new CustomerBasket(id));
+        }
+         [HttpPost]
+         public async Task<ActionResult<CustomerBasket>> UpdateBasket(CustomerBasket basket){
+            var updatedBasket = await _basketRepository.UpdateBasketAsync(basket);
+            return Ok(updatedBasket);
+        }
+        [HttpDelete]
+        public async Task DeleteBasket(string id){
+            await _basketRepository.DeleteBasketAsync(id);
+        }
+
+
+
+    }
+}
--- API/Startup.cs
+++ API/Startup.cs
@@ -33,7 +33,7 @@ namespace API
             //kết nối csdl
             services.AddDbContext<StoreContext>(x => x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
             //kết nối StackExchange.Redis => cai thu vien chon Infactructure
-            services.AddSingleton<ConnectionMultiplexer>(c =>
+            services.AddSingleton<IConnectionMultiplexer>(c =>
             {
                 var configuration = ConfigurationOptions.Parse(_config.GetConnectionString("Redis"),true);
                 return ConnectionMultiplexer.Connect(configuration);
