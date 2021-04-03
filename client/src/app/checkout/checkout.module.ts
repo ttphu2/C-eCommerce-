067fee787d0909b794e835f90db0b95fb92539d3
ttphu2 <ttphu34@gmail.com>
2021-04-03 17:31:17 +0700

    CLIENT 1. setup co ban checkout + @angular/material  + steppe form 3. Checkout-address

--- client/src/app/checkout/checkout.module.ts
+++ client/src/app/checkout/checkout.module.ts
@@ -2,14 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CheckoutComponent } from './checkout.component';
 import { CheckoutRoutingModule } from './checkout-routing.module';
-
-
+import { CheckoutAddressComponent } from './checkout-address/checkout-address.component';
+import { CheckoutReviewComponent } from './checkout-review/checkout-review.component';
+import { CheckoutPaymentComponent } from './checkout-payment/checkout-payment.component';
+import { CheckoutDeliveryComponent } from './checkout-delivery/checkout-delivery.component';
+import { CheckoutSuccessComponent } from './checkout-success/checkout-success.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
-  declarations: [CheckoutComponent],
+  declarations: [CheckoutComponent, CheckoutAddressComponent, CheckoutReviewComponent,
+    CheckoutPaymentComponent, CheckoutDeliveryComponent, CheckoutSuccessComponent],
   imports: [
     CommonModule,
-    CheckoutRoutingModule
+    CheckoutRoutingModule,
+    SharedModule
   ]
 })
 export class CheckoutModule { }
